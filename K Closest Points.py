527ef6e93efcc2eefc3d6c1bbbc4d99d9ac6ee84
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-27 00:46:39 -0400

    Update K Closest Points.py

--- K Closest Points.py
+++ K Closest Points.py
@@ -1,1 +1,35 @@
 #Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+from heapq import heapify, heappop
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        if not k or not points:
+            return None
+
+        def distance(x, y):
+            return sqrt(x**2 + y**2)
+
+        n = len(points)
+        print(n)
+
+        dis_dict = {}
+        heap = []
+        for each in points:
+            each_distance = distance(each[0], each[1])
+            heap.append(each_distance)
+            dis_dict[(each[0], each[1])] = each_distance
+            # dis_dict[each_distance] = each
+        print(dis_dict)
+        
+        heapify(heap)
+        print(heap)
+        res = set()
+
+        for i in range(k):
+            valu_dist = heappop(heap)
+            for key, value in dis_dict.items():
+                if value == valu_dist:
+                    res.add(key)
+            
+        print(res)
+
+        return res
