471cb257e938a415f842232ae7713f51ec9a1052
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-03-29 02:44:52 -0400

    Update K Closest Points.py

--- K Closest Points.py
+++ K Closest Points.py
@@ -1,40 +1,19 @@
 from heapq import heapify, heappop
+
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         if not k or not points:
-            return None
+            return []
 
         def distance(x, y):
-            return sqrt(x**2 + y**2)
+            return x**2 + y**2
 
-        n = len(points)
-        # print(n)
-
-        dis_dict = {}
-        heap = []
-        for each in points:
-            each_distance = distance(each[0], each[1])
-            heap.append(each_distance)
-            # dis_dict[(each[0], each[1])] = each_distance
-            if each_distance not in dis_dict.keys():
-                dis_dict[each_distance] = [each]
-            else:
-                dis_dict[each_distance].append(each)
-        # print(dis_dict)
-        
+        heap = [(distance(x, y), x, y) for x, y in points]
         heapify(heap)
-        # print("heap",  heap)
-        res_list = []
+
         res = []
+        for _ in range(k):
+            _, x, y = heappop(heap)
+            res.append([x, y])
 
-        for i in range(k):
-            valu_dist = heappop(heap)
-            # print(" ", valu_dist, dis_dict[valu_dist])
-            if dis_dict[valu_dist] not in res_list:
-                res_list.append(dis_dict[valu_dist])
-                
-                for each_pair in dis_dict[valu_dist]:
-                    res.append(each_pair)
-
-        # print(res)
         return res
