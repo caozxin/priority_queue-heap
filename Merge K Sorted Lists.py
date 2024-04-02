7dda304cea160e845aa385907508ec05c7adebbd
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2024-04-01 23:49:07 -0400

    Update Merge K Sorted Lists.py

--- Merge K Sorted Lists.py
+++ Merge K Sorted Lists.py
@@ -1,1 +1,3 @@
+#Merge k sorted chains (sequences are ascending sequences) and return the merged sorted chains (sequences are ascending sequences). Try to analyse and describe its complexity.
 
+#Analyze and describe its complexity
