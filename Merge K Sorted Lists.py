0c61507eebf3604d8c353dbb88fd9b04dacf9caf
caozxin <zcaolegend@gmail.com>
2024-05-02 21:52:16 -0400

    update description for Merge K Sorted Lists.py

--- Merge K Sorted Lists.py
+++ Merge K Sorted Lists.py
@@ -1,3 +1,10 @@
 #Merge k sorted chains (sequences are ascending sequences) and return the merged sorted chains (sequences are ascending sequences). Try to analyse and describe its complexity.
 
 #Analyze and describe its complexity
+"""
+Given k sorted lists of numbers, merge them into one sorted list.
+
+Input: [[1, 3, 5], [2, 4, 6], [7, 10]]
+
+Output: [1, 2, 3, 4, 5, 6, 7, 10]
+"""
