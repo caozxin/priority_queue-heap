f1a26c1f4c3ad1c2d98b0ef423718bad5abc4b37
caozxin <zcaolegend@gmail.com>
2024-05-07 00:13:49 -0400

    updated Merge K Sorted Lists.py

--- Merge K Sorted Lists.py
+++ Merge K Sorted Lists.py
@@ -8,7 +8,7 @@ Input: [[1, 3, 5], [2, 4, 6], [7, 10]]
 
 Output: [1, 2, 3, 4, 5, 6, 7, 10]
 """
-
+import heapq
 
 # Definition for singly-linked list.
 """
@@ -24,14 +24,38 @@ class Solution:
 
 
 from typing import List
+from heapq import heappop, heappush
 def merge_k_sorted_lists(lists: List[List[int]]) -> List[int]:
     # WRITE YOUR BRILLIANT CODE HERE
+    # def compare_each_list(list)
     print("result: ", lists)
     #None handling:
     if not lists:
         return []
 
-    # for each_sublist in lists
+    new_list = []
+    for each_sublist in lists:
+        print(each_sublist)
+        # each_sublist = heapq.heapify(each_sublist)
+        # each_sublist = heapq.heapify(each_sublist)
+
+        while len(each_sublist) > 0:
+
+            if len(new_list) == 0:
+                curr_node = heappop(each_sublist) # you need to pop the first one
+                new_list.append(curr_node)
+                print(new_list)
+            else:
+                curr_node = heappop(new_list)
+                compare_node = heappop(each_sublist)
+                print(curr_node, compare_node)
+                exit()
+                if curr_node < compare_node:
+                    new_list.append(curr_node)
+                    heappush(compare_node)
+                # else:
+
+
 
 
 input = [[1, 3, 5], [2, 4, 6], [7, 10]]
