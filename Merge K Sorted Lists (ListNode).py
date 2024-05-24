51a946a66f552f60d6c4702d8c280edfff982330
caozxin <zcaolegend@gmail.com>
2024-05-23 23:22:26 -0400

    updated Merge K Sorted Lists (ListNode).py

--- Merge K Sorted Lists (ListNode).py
+++ Merge K Sorted Lists (ListNode).py
@@ -1,53 +1,57 @@
 """
 Definition of ListNode
+"""
 class ListNode(object):
 
     def __init__(self, val, next=None):
         self.val = val
         self.next = next
-"""
-from heapq import heappop, heappush
 
+import heapq
+from heapq import heappop, heappush
 
 class Solution:
     """
     @param lists: a list of ListNode
     @return: The head of one sorted list.
     """
-
     def mergeKLists(self, lists):
         # write your code here
-
-        print("result: ", lists)
-        # None handling:
-        if not lists:
-            return []
-
-        res = []
         heap = []
-        n = len(lists)
+        res = ListNode(None)
 
-        for curr_list in lists:
-            # push first number of each list into the heap
-            print("curr_list", curr_list)
-            exit()
-            curr_heap = [heappop(curr_list), curr_list, 0]
-            print(curr_heap)
-            exit()
-            # heappush(heap, (heappop(current_list), current_list, 0))  # 1
+        def searchListNode(alist: ListNode, heap):
 
-        exit()
+            if not alist:
+                return
+
+            # print(alist.val)
+            heappush(heap, alist.val )
+            # print("heap", heap)
+
+            if alist.next:
+                # temp = alist.next
+                # alist.val = temp
+                searchListNode(alist.next, heap)
+
+        for each_list in lists:
+            searchListNode(each_list, heap)
+
+        # heap.sort()
+
+        # print("heap", heap)
+
+        # Initialize the dummy node
+        dummy = ListNode(None)
+        current = dummy
+
         while heap:
-            print("remaining lists: ", lists)
-            print("heap", heap)
-            curr_node = heappop(heap)
-            # print(curr_node)
-            res.append((curr_node[0]))
-            print(res)
-            if curr_node[1]:
-                current_list = curr_node[1]
-                # print(current_list)
-                # exit()
-                heappush(heap, (heappop(current_list), current_list, 0))
-                print("after", heap)
-        return res
+            curr_node = heapq.heappop(heap)
+            # print("curr_node:", curr_node)
+            current.next = ListNode(curr_node)
+            current = current.next
+
+        return dummy.next
+
+        # return res
+
